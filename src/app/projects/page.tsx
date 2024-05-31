88dbe1650f1de5a0b08bdb16e96235d30d0e4a1f
Oscar <oskharm12@gmail.com>
2024-05-31 16:22:20 -0300

    alteração no layout para telas menores

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -35,7 +35,6 @@ export default async function ProjectsPage() {
             <Navigation />
 
             <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-
                 <div className="max-w-2xl mx-auto lg:mx-0">
                     <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
                         Meus Projetos
@@ -45,7 +44,7 @@ export default async function ProjectsPage() {
                         já finalizados.
                     </p>
                 </div>
-                <div style={{ display: 'grid', gridTemplateColumns: 'auto auto auto ' }} >
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     {produtos.map((item) => (
                         <ProjectCard 
                             key={item.product_name}
@@ -59,7 +58,6 @@ export default async function ProjectsPage() {
                 </div>
                 <div className="hidden w-full h-px md:block bg-zinc-800" />
             </div>
-
         </div>
     );
 }
